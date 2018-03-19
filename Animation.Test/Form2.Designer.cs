5144cda9f8e66908bdbfa41b8c36c45d4ef67d9b
WLJ <932444208@qq.com>
2018-03-20 02:09:20 +0800

    winform 3d test

--- Animation.Test/Form2.Designer.cs
+++ Animation.Test/Form2.Designer.cs
@@ -28,115 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this._tbInput = new System.Windows.Forms.TextBox();
-            this._tbPattern = new System.Windows.Forms.TextBox();
-            this._tbReplacement = new System.Windows.Forms.TextBox();
-            this._btnReplace = new System.Windows.Forms.Button();
-            this._tbOutput = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // _tbInput
-            // 
-            this._tbInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this._tbInput.Location = new System.Drawing.Point(31, 17);
-            this._tbInput.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this._tbInput.Multiline = true;
-            this._tbInput.Name = "_tbInput";
-            this._tbInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this._tbInput.Size = new System.Drawing.Size(718, 360);
-            this._tbInput.TabIndex = 0;
-            // 
-            // _tbPattern
-            // 
-            this._tbPattern.Location = new System.Drawing.Point(140, 396);
-            this._tbPattern.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this._tbPattern.Name = "_tbPattern";
-            this._tbPattern.Size = new System.Drawing.Size(238, 23);
-            this._tbPattern.TabIndex = 1;
-            // 
-            // _tbReplacement
-            // 
-            this._tbReplacement.Location = new System.Drawing.Point(140, 435);
-            this._tbReplacement.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this._tbReplacement.Name = "_tbReplacement";
-            this._tbReplacement.Size = new System.Drawing.Size(238, 23);
-            this._tbReplacement.TabIndex = 2;
-            // 
-            // _btnReplace
-            // 
-            this._btnReplace.Location = new System.Drawing.Point(427, 396);
-            this._btnReplace.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this._btnReplace.Name = "_btnReplace";
-            this._btnReplace.Size = new System.Drawing.Size(74, 28);
-            this._btnReplace.TabIndex = 4;
-            this._btnReplace.Text = "replace";
-            this._btnReplace.UseVisualStyleBackColor = true;
-            this._btnReplace.Click += new System.EventHandler(this._tbReplace_Click);
-            // 
-            // _tbOutput
-            // 
-            this._tbOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this._tbOutput.Location = new System.Drawing.Point(31, 476);
-            this._tbOutput.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this._tbOutput.Multiline = true;
-            this._tbOutput.Name = "_tbOutput";
-            this._tbOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this._tbOutput.Size = new System.Drawing.Size(718, 218);
-            this._tbOutput.TabIndex = 5;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(78, 402);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 17);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "pattern";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(50, 441);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 17);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "replacement";
-            // 
-            // Form2
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(778, 713);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this._tbOutput);
-            this.Controls.Add(this._btnReplace);
-            this.Controls.Add(this._tbReplacement);
-            this.Controls.Add(this._tbPattern);
-            this.Controls.Add(this._tbInput);
-            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.Name = "Form2";
-            this.Text = "Replacement";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            this.Text = "Form2";
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox _tbInput;
-        private System.Windows.Forms.TextBox _tbPattern;
-        private System.Windows.Forms.TextBox _tbReplacement;
-        private System.Windows.Forms.Button _btnReplace;
-        private System.Windows.Forms.TextBox _tbOutput;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
     }
 }
--- Animation.Test/Form2.cs
+++ Animation.Test/Form2.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace Animation.Test
@@ -15,25 +15,200 @@ namespace Animation.Test
         public Form2()
         {
             InitializeComponent();
+            base.Load += new EventHandler(this.Form2_Load);
+            base.Paint += new PaintEventHandler(this.Form2_Paint);
+            base.Resize += new EventHandler(this.Form2_Resize);
         }
 
-        private void _tbReplace_Click(object sender, EventArgs e)
-        {
-            String input = this._tbInput.Text.Trim();
-            String pattern = this._tbPattern.Text.Trim();
-            String replacement = this._tbReplacement.Text.Trim();
-            try
-            {
-                this._tbOutput.Text = Regex.Replace(input, pattern, replacement);
-            }
-            catch (Exception exc)
-            {
-                this._tbOutput.Text = null;
-                MessageBox.Show(exc.Message);
-            }
-          
-         
+ 
 
+        private void DrawCone(Graphics g, Point xy1, float r)
+        {
+            float height = g.VisibleClipBounds.Height;
+            RectangleF rect = new RectangleF(xy1.X - r, height - ((float)(xy1.Y - (1.49 * r))), 2f * r, r);
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(rect, 180f, -180f);
+            using (LinearGradientBrush brush = new LinearGradientBrush(rect, Color.Gray, Color.WhiteSmoke, 190f))
+            {
+                g.FillPath(brush, path);
+                path.Reset();
+                Point point = xy1;
+                point.Offset((int)Math.Round((double)-r), (int)Math.Round((double)(-2f * r)));
+                Point point2 = xy1;
+                point2.Offset((int)Math.Round((double)r), (int)Math.Round((double)(-2f * r)));
+                PointF[] thePolygon = new PointF[] { point, xy1, point2 };
+                this.ReverseY(ref thePolygon, height);
+                path.AddLines(thePolygon);
+                g.FillPath(brush, path);
+            }
+        }
+
+        private void DrawCube(Graphics g, float w, float h, float d, Point xy1)
+        {
+            Point point=Point.Empty;
+            float height = g.VisibleClipBounds.Height;
+            GraphicsPath path = new GraphicsPath();
+            d *= 0.7f;
+            Graphics graphics = g;
+            using (SolidBrush brush = new SolidBrush(Color.LightGray))
+            {
+                graphics.FillRectangle(brush, (float)xy1.X, height - xy1.Y, w, h);
+            }
+       
+            point.X = (int)Math.Round((double)(xy1.X + (d * Math.Cos(0.52356020942408377))));
+            point.Y = (int)Math.Round((double)(xy1.Y + (d * Math.Sin(0.52356020942408377))));
+            Point point2 = point;
+            point2.Offset((int)Math.Round((double)w), 0);
+            Point point3 = xy1;
+            point3.Offset((int)Math.Round((double)w), 0);
+            PointF[] thePolygon = new PointF[] { xy1, point, point2, point3 };
+            this.ReverseY(ref thePolygon, height);
+            path.AddLines(thePolygon);
+            using (PathGradientBrush brush2 = new PathGradientBrush(path))
+            {
+                brush2.CenterPoint = new PointF(2f * w, height);
+                brush2.CenterColor = Color.SteelBlue;
+                graphics.FillPath(brush2, path);
+            }
+  
+            point = point3;
+            point3 = point2;
+            point3.Offset(0, (int)Math.Round((double)-h));
+            Point point4 = point;
+            point4.Offset(0, (int)Math.Round((double)-h));
+            thePolygon = new PointF[] { point, point2, point3, point4 };
+            this.ReverseY(ref thePolygon, height);
+            path.Reset();
+            path.AddLines(thePolygon);
+            using (PathGradientBrush brush3 = new PathGradientBrush(path))
+            {
+                brush3.CenterPoint = new PointF(2f * w, height);
+                brush3.CenterColor = Color.SlateGray;
+                graphics.FillPath(brush3, path);
+            }
+            goto DIS;
+DIS:
+            graphics = null;
+        }
+
+        private void DrawCylinder(Graphics g, Point xy1, int h, float r)
+        {
+            float height = g.VisibleClipBounds.Height;
+            RectangleF rect = new RectangleF(xy1.X - r, height - ((float)((xy1.Y + (((double)r) / 1.9)) - h)), 2f * r, r);
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(rect, 180f, -180f);
+            using (LinearGradientBrush brush = new LinearGradientBrush(rect, Color.Gray, Color.WhiteSmoke, 180f))
+            {
+                g.FillPath(brush, path);
+                path.Reset();
+                Point point = xy1;
+                point.Offset((int)Math.Round((double)-r), 0);
+                Point point2 = point;
+                point2.Offset(0, 0 - h);
+                Point point3 = xy1;
+                point3.Offset((int)Math.Round((double)r), 0);
+                Point point4 = point3;
+                point4.Offset(0, 0 - h);
+                PointF[] thePolygon = new PointF[] { point2, point, point3, point4 };
+                this.ReverseY(ref thePolygon, height);
+                path.AddLines(thePolygon);
+                g.FillPath(brush, path);
+            }
+            path.Reset();
+            rect.Offset(0f, (float)(0 - h));
+            path.AddEllipse(rect);
+            using (PathGradientBrush brush2 = new PathGradientBrush(path))
+            {
+                brush2.CenterPoint = new PointF(xy1.X + r, height - r);
+                brush2.CenterColor = Color.Gray;
+                g.FillPath(brush2, path);
+            }
+        }
+
+        private void DrawGrid(Graphics g, float theScale)
+        {
+            float height = g.VisibleClipBounds.Height;
+            float emSize = theScale / 25f;
+            using (Font font = new Font("Arial", emSize))
+            {
+                using (Pen pen = new Pen(Color.SkyBlue, theScale / 500f))
+                {
+                    using (SolidBrush brush = new SolidBrush(Color.LightGray))
+                    {
+                        float y = height - ((float)(1.5 * emSize));
+                        float num5 = theScale;
+                        for (float i = 0f; i <= num5; i += 5f)
+                        {
+                            g.DrawLine(pen, i, height - 0f, i, height - theScale);
+                            g.DrawString(i.ToString(), font, brush, i, y);
+                            float num4 = height - (((float)(1.5 * emSize)) + i);
+                            g.DrawLine(pen, 0f, height - i, theScale, height - i);
+                            g.DrawString(i.ToString(), font, brush, 0f, num4);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void DrawSphere(Graphics g, float x, float y, float r)
+        {
+            float height = g.VisibleClipBounds.Height;
+            RectangleF rect = new RectangleF(x - r, height - (y + r), 2f * r, 2f * r);
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(rect);
+            rect.X += r / 50f;
+            rect.Y += r / 50f;
+            g.DrawEllipse(new Pen(Color.LightGray, r / 16f), rect);
+            using (PathGradientBrush brush = new PathGradientBrush(path))
+            {
+                brush.CenterPoint = new PointF(x + r, height - 10f);
+                brush.CenterColor = Color.Gray;
+                Color[] colorArray = new Color[] { Color.WhiteSmoke };
+                brush.SurroundColors = colorArray;
+                g.FillPath(brush, path);
+                brush.CenterColor = Color.LightGray;
+                rect.Width *= 0.2f;
+                rect.Height *= 0.2f;
+                rect.Offset(r / 3f, r / 2f);
+                g.FillEllipse(brush, rect);
+            }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.DoubleBuffered = true;
+        }
+
+        private void Form2_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            float theScale = 35f;
+            float sx = ((float)base.ClientSize.Width) / theScale;
+           // graphics.ScaleTransform(sx, sx);
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            graphics.Clear(Color.Black);
+         //   this.DrawGrid(e.Graphics, theScale);
+            this.DrawCube(e.Graphics, 300f, 500f, 100f, new Point(14,550));
+            //this.DrawSphere(e.Graphics, 9f, 15f, 6f);
+            //this.DrawCone(e.Graphics, new Point(0x1a, 0x15), 6f);
+            //this.DrawCylinder(e.Graphics, new Point(0x11, 0x11), 10, 4f);
+            graphics = null;
+        }
+
+        private void Form2_Resize(object sender, EventArgs e)
+        {
+            base.Invalidate();
+        }
+
+      
+
+        private void ReverseY(ref PointF[] thePolygon, float yoffset)
+        {
+            int num = thePolygon.Length - 1;
+            for (int i = 0; i <= num; i++)
+            {
+                thePolygon[i].Y = yoffset - thePolygon[i].Y;
+            }
         }
     }
 }
